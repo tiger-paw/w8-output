31262b2202a123739fac245aba064a7d676603a7
idetanaoya <118895979+regal-tiger-paw@users.noreply.github.com>
2025-02-22 14:39:13 +0900

    課題の完了

---
+++ database/app.js
@@ -0,0 +1,57 @@
+if(('localStorage' in window) && (window.localStorage !== null)) {
+	console.log("ローカルストレージが使える");
+} else {
+	console.log("ローカルストレージが使えない");
+}
+
+const $submit = document.getElementById("submit-button2");
+const $emailInput = document.getElementById("email2");
+const $passwordInput = document.getElementById("password2")
+
+window.addEventListener("load", () => {
+	const savedEmail = localStorage.getItem('access_email');
+	const savedPassword = localStorage.getItem('access_password');
+
+	if(savedEmail) {
+		$emailInput.value = savedEmail;
+	}
+	if(savedPassword) {
+		$passwordInput.value = savedPassword;
+	}
+});
+
+
+
+$submit.addEventListener("click", () => {
+	const email = $emailInput.value;
+	const password = $passwordInput.value;
+
+	// ローカルストレージにemailとpasswordを保存
+	localStorage.setItem('access_email', email);
+	localStorage.setItem('access_password', password);
+
+})
+
+
+// // localStorageを使う
+
+// var email = 1;
+
+// // データの保存
+// localStorage.setItem('access_email', num);
+// window.localStorage.setItem('access_email', num);
+// localStorage.access_email = num
+
+
+// // データの取得
+// num = localStorage.getItem('access_email');
+// num = window.localStorage.getItem('access_email');
+// num = localStorage.access_email
+
+// // データの削除
+// localStorage.removeItem('access_email');
+// window.localStorage.removeItem('access_email');
+
+// // localStorageを初期化
+// localStorage.clear();
+// window.localStorage.clear();
